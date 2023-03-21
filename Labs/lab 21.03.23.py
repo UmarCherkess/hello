379642e09c63416074f0d79f672e6b3be9f46372
UmarCherkess <92584242+UmarCherkess@users.noreply.github.com>
2023-03-21 09:22:50 +0300

    Update lab 21.03.23.py

--- Labs/lab 21.03.23.py
+++ Labs/lab 21.03.23.py
@@ -9,3 +9,21 @@ print(result.group(0))
 import re 
 result = re.sub(r'Alabama','Omaha','Sweet home Alabama')
 print(result)
+
+#Задание 3
+
+import re 
+result = re.findall(r'Bulka','Bulka s makom and Bulka s koritsey')
+print(result)
+
+#Задание 4
+
+import re
+result = re.match(r'Mr','Mr boombastic')
+print(result.group(0))
+
+#Задание 5
+
+import re
+result = re.split(r'a','Sweet home Alabama', maxsplit = 2) 
+print(result)
